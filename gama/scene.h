2f48643c80874437d72a35b77e14a6dba5a225e4
Engon Ken Morel <engonken8@gmail.com>
2025-08-26 20:18:44 +0100

    initial commit

---
+++ gama/scene.h
@@ -0,0 +1,79 @@
+#ifndef GAMA_SCENE_INCLUDED
+#define GAMA_SCENE_INCLUDED
+
+#include "color.h"
+#include "events.h"
+#include "shape.h"
+#include "sprite.h"
+
+struct sApp;
+
+typedef struct sScene {
+  SpriteList sprites;
+  ShapeList shapes;
+  Color background;
+  struct sApp *app;
+  void (*create)(struct sScene *);
+  void (*update)(struct sScene *, double);
+  void (*render)(struct sScene *);
+  void (*destroy)(struct sScene *);
+
+  void (*onclick)(struct sScene *, MouseClickEvent *);
+  void (*onkey)(struct sScene *, KeyEvent *);
+
+} Scene;
+
+Scene *createScene(struct sApp *app) {
+  Scene *s = (Scene *)malloc(sizeof(Scene));
+  if (s == NULL)
+    return NULL;
+  s->sprites.sprites = NULL;
+  s->shapes.shapes = NULL;
+  createSpriteList(&s->sprites);
+  createShapeList(&s->shapes);
+  s->app = app;
+  s->create = NULL;
+  s->update = NULL;
+  s->render = NULL;
+  s->destroy = NULL;
+  s->onclick = NULL;
+  s->onkey = NULL;
+  s->background = LIGHTCYAN;
+  return s;
+}
+
+void addShapeToScene(Scene *scene, Shape *shape) {
+  addShapeToList(&scene->shapes, shape);
+}
+void addSpriteToScene(Scene *scene, Sprite *sprite) {
+  addSpriteToList(&scene->sprites, sprite);
+}
+
+void sceneCreate(Scene *s) {
+  if (s->create != NULL)
+    s->create(s);
+}
+void sceneUpdate(Scene *s, double theta) {
+  if (s->update != NULL)
+    s->update(s, theta);
+}
+void sceneRender(Scene *s) {
+  SetClearColor(s->background);
+  if (s->render != NULL)
+    s->render(s);
+}
+void sceneDestroy(Scene *s) {
+  if (s->destroy != NULL)
+    s->destroy(s);
+}
+
+void sceneClick(Scene *s, MouseClickEvent *e) {
+  if (s->onclick != NULL)
+    s->onclick(s, e);
+}
+void sceneKey(Scene *s, KeyEvent *e) {
+  if (s->onkey != NULL)
+    s->onkey(s, e);
+}
+
+#endif
